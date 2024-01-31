7d046eccfc4e4dbfe94af2e8390a941ee53a98d0
Antooh123 <tonioskirts@gmail.com>
2024-01-31 03:39:01 -0800

    Stop tracking untracked files

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,12 +2,6 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print("{}is positive".format(number))
+    print(f"{number} is positive")
 elif number == 0:
-    print("{}is zero".format(number))
-else:
-<<<<<<< HEAD
-    print("{}is negative".format(number))
-=======
-    print(f"{number} is negative")
->>>>>>> 499c7e1a6732e0051ca86e032776507d2026a7dc
+    print(f"{number} is zero")
--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
+for i in range(ord('a'), ord("z")+1):
+    if chr(i) not in ['q', 'e']:
+        print("{0}" .format(chr(i)), end='')!= 'e' and chr(i) != 'q':
         print('{:c}'.format(i), end='')
