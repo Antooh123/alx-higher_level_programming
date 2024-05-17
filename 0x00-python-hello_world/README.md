223e5dcd1ddc5b444c0a824c2aa8b03b0f0280c4
Antooh123 <tonioskirts@gmail.com>
2024-05-17 03:49:43 -0700

    Removed test directory

--- 0x00-python-hello_world/README.md
+++
@@ -1,1 +0,0 @@
-This is python hello world task
--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-for i in range(ord('a'), ord("z")+1):
-    if chr(i) not in ['q', 'e']:
-<<<<<<< HEAD
-        print("{0}" .format(chr(i)), end='')!= 'e' and chr(i) != 'q':
-        print('{:c}'.format(i), end='')
-=======
-        print("{0}" .format(chr(i)), end='')
->>>>>>> 36a3c72003a9a6ab3dc666f6890350cabc013183
